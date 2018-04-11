2dd288e94f13664c98b76af6e9f29d26e39e9e1f
T0067 Zhang Wuyang <T0067@HDGE.com>
2018-04-11 15:27:33 +0800

    update

--- tf000.py
+++ tf000.py
@@ -48,7 +48,7 @@ def cnn_model_fn(features, labels, mode):
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-        train = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
     eval_metric_ops = {"accuracy":tf.metrics.accuracy(labels=labels, predictions=predictions["class"])}
